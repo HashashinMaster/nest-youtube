030e72fcda7a44a0be827e7a7b017440e9801be0
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-06-28 00:31:31 +0200

    get video info by id

--- src/youtube/youtube.controller.ts
+++ src/youtube/youtube.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('/api/youtube')
-export class YoutubeController {}
+import { Controller, Get, Post, Res, Param } from "@nestjs/common";
+import { Response } from "express";
+import { execFile } from "child_process";
+import { promisify } from "util";
+import { join } from "path";
+@Controller("/api/youtube")
+export class YoutubeController {
+  @Post("/:videoId")
+  async getVideo(@Param() params: { videoId: string }, @Res() res: Response) {
+    const videoUrl = `https://www.youtube.com/watch?v=${params.videoId}`;
+    const exec = promisify(execFile);
+    console.log(params);
+    const { stdout, stderr } = await exec(
+      join(__dirname, "..", "..", "yt-dlp.exe"),
+      ["--dump-json", "--no-playlist", videoUrl],
+    );
+    if (stderr) {
+      console.log(stderr);
+      return res.sendStatus(500);
+    } else {
+      return res.json({
+        success: true,
+        data: JSON.parse(stdout),
+      });
+    }
+  }
+}
