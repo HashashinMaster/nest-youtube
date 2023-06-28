2b6832155a2396eecbac86fb25971fd10887f392
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-06-28 19:05:23 +0200

    add comments

--- src/youtube/youtube.controller.ts
+++ src/youtube/youtube.controller.ts
@@ -4,21 +4,31 @@ import { execFile } from "child_process";
 import { promisify } from "util";
 import { join } from "path";
 
+// making execFile async by wraping it inside promise
+const exec = promisify(execFile);
+
 @Controller("/api/youtube")
 export class YoutubeController {
-  @Post("/:videoId")
+  @Post("video/:videoId")
   async getVideo(@Param() params: { videoId: string }, @Res() res: Response) {
+    //generate video url by video id
     const videoUrl = `https://www.youtube.com/watch?v=${params.videoId}`;
-    const exec = promisify(execFile);
-    console.log(params);
+    /**
+     * execute yd-dlp with options:
+     * --dumup-json: return video data as json
+     * --no-playlist: return video only if the video in a playlist
+     * videoUrl: the url of the video
+     */
     const { stdout, stderr } = await exec(
       join(__dirname, "..", "..", "yt-dlp.exe"),
       ["--dump-json", "--no-playlist", videoUrl],
     );
+    //display error if exec func fails
     if (stderr) {
       console.log(stderr);
       return res.sendStatus(500);
     } else {
+      //send data
       return res.json({
         success: true,
         data: JSON.parse(stdout),
