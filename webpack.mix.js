69f8c402e7fa380a434e3a65982f54f663189731
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-06-29 00:47:34 +0200

    jsoneditor compilation config

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,12 @@ let mix = require("laravel-mix");
 
 mix
   .ts("resources/ts/app.ts", "src/public/js/app.js")
-  .sass("resources/css/app.scss", "src/public/css/app.css");
+  .sass("resources/css/app.scss", "src/public/css/app.css")
+  .copy(
+    "node_modules/jsoneditor/dist/jsoneditor.min.css",
+    "src/public/css/json-editor.css",
+  )
+  .copy(
+    "node_modules/jsoneditor/dist/img/jsoneditor-icons.svg",
+    "src/public/css/img/jsoneditor-icons.svg",
+  );
