87baf075855af7c6d98eafa9816434eac6d91f68
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-06-28 21:14:13 +0200

    remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { YoutubeApiController } from "./youtube-api/youtube-api.controller";
-import { YoutubeController } from './youtube/youtube.controller';
+import { YoutubeController } from "./youtube/youtube.controller";
 
 @Module({
   imports: [],
-  controllers: [AppController, YoutubeApiController, YoutubeController],
+  controllers: [YoutubeApiController, YoutubeController],
   providers: [AppService],
 })
 export class AppModule {}
