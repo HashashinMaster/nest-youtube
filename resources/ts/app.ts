7db6f8452573f21b5cc6d27fd65cb2c1bb208c0f
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-06-29 00:49:22 +0200

    jsoneditor type definition

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,6 +1,6 @@
 import Alpine from "alpinejs";
 import axios from "axios";
-import { JSONEditor } from "vanilla-jsoneditor";
+import JSONEditor from "jsoneditor";
 
 window.Alpine = Alpine;
 window.axios = axios;
