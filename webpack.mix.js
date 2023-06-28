5ce7da7fb2b3876cb69ab69907103497e5574f3d
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-06-28 21:55:00 +0200

    bulma configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,5 @@
 let mix = require("laravel-mix");
 
-mix.ts("resources/ts/app.ts", "public/js/app.js");
+mix
+  .ts("resources/ts/app.ts", "public/js/app.js")
+  .sass("resources/css/app.scss", "public/css/app.css");
