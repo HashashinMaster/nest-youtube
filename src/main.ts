835a012c433387fa7a5b883b27cce50b63bf08cf
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-06-28 22:09:57 +0200

    filw atructure change

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
-import { join } from "path";
+import { resolve } from "path";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, "..", "public"));
-  app.setBaseViewsDir(join(__dirname, "..", "views"));
+  console.log(resolve("./src/public"));
+  app.useStaticAssets(resolve("./src/public"));
+  app.setBaseViewsDir(resolve("./src/views"));
   app.setViewEngine("hbs");
   await app.listen(3000);
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require("laravel-mix");
 
 mix
-  .ts("resources/ts/app.ts", "public/js/app.js")
-  .sass("resources/css/app.scss", "public/css/app.css");
+  .ts("resources/ts/app.ts", "src/public/js/app.js")
+  .sass("resources/css/app.scss", "src/public/css/app.css");
