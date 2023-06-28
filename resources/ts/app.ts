4c3dc7ef1adf3538493189987ae07da5d5dc8425
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-06-28 23:41:09 +0200

    change to JSONEditor

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,6 +1,8 @@
 import Alpine from "alpinejs";
 import axios from "axios";
+import { JSONEditor } from "vanilla-jsoneditor";
+
 window.Alpine = Alpine;
 window.axios = axios;
-
+window.JSONEditor = JSONEditor;
 Alpine.start();
--- resources/ts/index.d.ts
+++ resources/ts/index.d.ts
@@ -1,8 +1,11 @@
 import Alpine from "alpinejs";
 import axios from "axios";
+import { JSONEditor } from "vanilla-jsoneditor";
+
 declare global {
   interface Window {
     Alpine: Alpine;
     axios: axios;
+    JSONEditor: JSONEditor;
   }
 }
