0d60c3a1882ddd641c8960e12fd8a986897c2447
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-06-29 20:11:13 +0200

    isolate alpine components to external file

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -5,4 +5,19 @@ import JSONEditor from "jsoneditor";
 window.Alpine = Alpine;
 window.axios = axios;
 window.JSONEditor = JSONEditor;
+
+window.searchComponent = () => {
+  return {
+    search: "",
+    async getVideoJSON() {
+      console.log("starting");
+      const { data } = await axios.post(`/api/youtube/video/${this.search}`);
+      console.log(data);
+      const container = document.getElementById("json-viewer");
+      const editor = new JSONEditor(container);
+      editor.set(data);
+      return data;
+    },
+  };
+};
 Alpine.start();
--- resources/ts/index.d.ts
+++ resources/ts/index.d.ts
@@ -2,10 +2,15 @@ import Alpine from "alpinejs";
 import axios from "axios";
 import { JSONEditor } from "jsoneditor";
 
+interface SearchComponentProps {
+  search: string;
+  getVideoJSON: () => Promise<any>;
+}
 declare global {
   interface Window {
     Alpine: Alpine;
     axios: axios;
     JSONEditor: JSONEditor;
+    searchComponent: () => SearchComponentProps;
   }
 }
