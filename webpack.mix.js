70c024c77f859c2b469555d19bb188aadd88f987
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-02 21:50:23 +0200

    change folders structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,13 @@
 let mix = require("laravel-mix");
 
 mix
-  .ts("resources/ts/app.ts", "src/public/js/app.js")
-  .sass("resources/css/app.scss", "src/public/css/app.css")
+  .ts("resources/ts/app.ts", "public/js/app.js")
+  .sass("resources/css/app.scss", "public/css/app.css")
   .copy(
     "node_modules/jsoneditor/dist/jsoneditor.min.css",
-    "src/public/css/json-editor.css",
+    "public/css/json-editor.css",
   )
   .copy(
     "node_modules/jsoneditor/dist/img/jsoneditor-icons.svg",
-    "src/public/css/img/jsoneditor-icons.svg",
+    "public/css/img/jsoneditor-icons.svg",
   );
