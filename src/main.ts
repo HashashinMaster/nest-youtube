e937d91d70e08806f7aa8bf7283f46ea77f95b72
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-02 21:18:05 +0200

    configure nunjucks

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
+import * as nunjucks from "nunjucks";
 import { resolve } from "path";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  console.log(resolve("./src/public"));
-  app.useStaticAssets(resolve("./src/public"));
-  app.setBaseViewsDir(resolve("./src/views"));
-  app.setViewEngine("hbs");
+  const express = app.getHttpAdapter().getInstance();
+  const views = resolve("./views");
+  console.log(views);
+  nunjucks.configure(views, { express, watch: true });
+
+  app.useStaticAssets(resolve("./public"));
+  app.setBaseViewsDir(views);
+  app.setViewEngine("njk");
   await app.listen(3000);
 }
 bootstrap();
