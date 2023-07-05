cbe70f3515b309ec31a2a1de6cbc38e2bc8cc6ed
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-05 23:11:08 +0200

    add user socket gateway provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { YoutubeApiController } from "./youtube-api/youtube-api.controller";
 import { YoutubeController } from "./youtube/youtube.controller";
+import { UserGateway } from './user/user.gateway';
 
 @Module({
   imports: [],
   controllers: [YoutubeApiController, YoutubeController],
-  providers: [AppService],
+  providers: [AppService, UserGateway],
 })
 export class AppModule {}
