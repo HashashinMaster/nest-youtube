b848c50883a16eeed4dce2ae088acdb4899a04db
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-05 23:12:08 +0200

    generate uuid for user

--- src/youtube/youtube.controller.ts
+++ src/youtube/youtube.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Render, Body, Param } from "@nestjs/common";
+import { v4 as uuidv4 } from "uuid";
 
 @Controller()
 export class YoutubeController {
@@ -7,12 +8,12 @@ export class YoutubeController {
   index(@Param("type") type: string) {
     return {
       type,
+      uuid: uuidv4(),
     };
   }
   @Post("/component")
   @Render("components/video-card")
   showVideos(@Body() videosBody: { videos: Array<any> }) {
-    console.log(videosBody);
     return {
       videos: videosBody,
     };
