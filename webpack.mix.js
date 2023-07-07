2cdf6b5ce441c0d78528522cd86a0571068dbf3c
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-07 17:47:21 +0200

    add fontawsome script

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require("laravel-mix");
 
 mix
   .ts("resources/ts/app.ts", "public/js/app.js")
+  .ts("resources/ts/fontawsome.ts", "public/js/fontawsome.js")
   .sass("resources/css/app.scss", "public/css/app.css")
   .copy(
     "node_modules/jsoneditor/dist/jsoneditor.min.css",
