fc18e9cdeb57fb5dc955cd865f48364ecbe9f18b
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-08 18:42:37 +0200

    change to paths to dist path

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,16 @@ import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
 import * as nunjucks from "nunjucks";
-import { resolve } from "path";
+import { join } from "path";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const express = app.getHttpAdapter().getInstance();
-  const views = resolve("./views");
-  console.log(views);
+  const views = join(__dirname, "..", "views");
+  const staticFolder = join(__dirname, "..", "public");
+  console.log();
   nunjucks.configure(views, { express, watch: true });
 
-  app.useStaticAssets(resolve("./public"));
+  app.useStaticAssets(staticFolder);
   app.setBaseViewsDir(views);
   app.setViewEngine("njk");
   await app.listen(3000);
